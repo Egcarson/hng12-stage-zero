d8cabe19f4a8e44032b0eb0539442bd26582c39b
prevail <esehgodprevail@gmail.com>
2025-01-29 11:02:10 +0100

    updated the main.py file, added root endpoint

--- main.py
+++ main.py
@@ -22,6 +22,11 @@ stage_0_db = {
     "github_url": "<https://github.com/Egcarson/hng12-stage-zero.git>"
 }
 
+#root
+@app.get('/')
+async def root():
+    return {"message": "Welcome to HNG Internship Stage 0"}
+
 @app.get("/stage-zero", status_code=status.HTTP_200_OK, response_model=dict)
 async def get_stage_zero_info():
     return stage_0_db
